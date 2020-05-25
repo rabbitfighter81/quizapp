bf356ca5c0b76834af046d2f81c7f4e6ab6b8ad2
Joshua Michael <joshua@muthr.local>
2020-05-25 13:07:45 -0700

    feat/tutorial-setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: HomeScreen(),
-      ),
+          body: ListView(
+        scrollDirection: Axis.horizontal,
+        children: _cards(),
+      )),
     );
+  }
+
+  List<Widget> _cards() {
+    return [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        .map((v) => Container(
+              color: Colors.lightGreen,
+              margin: EdgeInsets.all(20),
+              height: 100,
+              child: Text('$v'),
+            ))
+        .toList();
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({
-    Key key,
-  }) : super(key: key);
+class MyFirstWidget extends StatefulWidget {
+  MyFirstWidget({Key key}) : super(key: key);
+
+  @override
+  _MyFirstWidgetState createState() => _MyFirstWidgetState();
+}
+
+class _MyFirstWidgetState extends State<MyFirstWidget> {
+  int count = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Hello World!'),
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: FlatButton(
+            color: Colors.grey,
+            textColor: Colors.white,
+            onPressed: () {
+              setState(() {
+                count++;
+              });
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('$count'),
+            ),
+          ),
+        ),
       ),
     );
   }
